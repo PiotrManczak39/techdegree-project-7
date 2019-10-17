29dc558204d511f9f66f97229f80bf6881e39049
Piotr Manczak <51323846+PiotrManczak39@users.noreply.github.com>
2019-10-17 18:13:18 +0100

    Rough sketch without one chart

--- web_app_dashboard_v3.1/js/chart.js
+++ web_app_dashboard_v3.1/js/chart.js
@@ -1,19 +1,59 @@
-var ctx = document.getElementById('myChart').getContext('2d');
+const alertButton = document.querySelector('.alert button');
+const alertBox = document.querySelector('.alert-box');
+
+alertButton.addEventListener('click', () => {
+    alertBox.style.display = 'none';
+});
+
+//main chart
+var ctx = document.getElementById('mainChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'line',
-
-    // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ["", "16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+    "4-10", "11-17", "18-24", "25-31"],
         datasets: [{
-            label: 'My First dataset',
-            backgroundColor: '#729EC9',
-            borderColor: '#729EC9',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            label: '# of Hits',
+            backgroundColor: 'rgba(116, 119, 191, .3)',
+            borderColor: 'blue',
+            data: [0, 750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 2250],
+            borderWidth: 1,
+            pointHoverBorderWidth: 3,
+            pointHoverBorderCOlor: '#000'
         }]
     },
-
-    // Configuration options go here
-    options: {}
+    options: {
+      title: {
+        display: true,
+        text: 'TRAFFIC',
+        position: 'top'
+      },
+      legend: {
+        display: false
+      }
+    }
 });
+
+//bar chart
+var barChart = document.getElementById('barChart').getContext('2d');
+var chart2 = new Chart(barChart, {
+    // The type of chart we want to create
+    type: 'bar',
+    data: {
+      labels: ["S", "M", "T", "W", "T", "F", "S"],
+      datasets: [{
+          label: '# of Hits',
+          data: [75, 115, 175, 125, 225, 200, 100],
+          backgroundColor: '#7477BF',
+          borderWidth: 1
+      }]
+    },
+    options: {
+      legend: {
+        display: false
+      }
+    }
+});
+
+//doughnut chart
